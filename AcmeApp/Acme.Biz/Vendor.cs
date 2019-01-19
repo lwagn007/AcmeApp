9dc96874ec13bd9803db3eadc1ecc8daa10bd21f
Lawrence Wagner <lawrencewagner77@gmail.con>
2019-01-19 13:43:42 -0500

    chaining methods

--- AcmeApp/Acme.Biz/Vendor.cs
+++ AcmeApp/Acme.Biz/Vendor.cs
@@ -24,27 +24,7 @@ namespace Acme.Biz
         /// <returns></returns>
         public OperationResult PlaceOrder(Product product, int quantity)
         {
-            if (product == null)
-                throw new ArgumentNullException(nameof(product));
-            if (quantity <= 0)
-                throw new ArgumentOutOfRangeException(nameof(quantity));
-
-            var success = false;
-
-            var orderText = "Order from Acme, Inc" + System.Environment.NewLine +
-                "Product: " + product.ProductCode + System.Environment.NewLine +
-                "Quantity: " + quantity;
-
-            var emailService = new EmailService();
-
-            var confirmation = emailService.SendMessage("New Order", orderText, this.Email);
-
-            if (confirmation.StartsWith("Message sent:"))
-                success = true;
-
-            var operationResult = new OperationResult(success, orderText);
-
-            return operationResult;
+            return PlaceOrder(product, quantity, null, null);
         }
 
         /// <summary>
@@ -56,33 +36,7 @@ namespace Acme.Biz
         /// <returns></returns>
         public OperationResult PlaceOrder(Product product, int quantity, DateTimeOffset? deliverBy)
         {
-            if (product == null)
-                throw new ArgumentNullException(nameof(product));
-            if (quantity <= 0)
-                throw new ArgumentOutOfRangeException(nameof(quantity));
-            if (deliverBy <= DateTimeOffset.Now)
-                throw new ArgumentOutOfRangeException(nameof(deliverBy));
-
-            var success = false;
-
-            var orderText = "Order from Acme, Inc" + System.Environment.NewLine +
-                "Product: " + product.ProductCode + System.Environment.NewLine +
-                "Quantity: " + quantity;
-            if (deliverBy.HasValue)
-            {
-                orderText += System.Environment.NewLine + "Deliver By: " + deliverBy.Value.ToString("d");
-            }
-
-            var emailService = new EmailService();
-
-            var confirmation = emailService.SendMessage("New Order", orderText, this.Email);
-
-            if (confirmation.StartsWith("Message sent:"))
-                success = true;
-
-            var operationResult = new OperationResult(success, orderText);
-
-            return operationResult;
+            return PlaceOrder(product, quantity, deliverBy, null);
         }
 
         /// <summary>
